ae1df229c8d4a869e15d4dd3e9102ff20409ad5a
RiccardoMarino00 <riccardo.marino09@gmail.com>
2024-03-06 15:52:01 +0100

    Added: multipli di 3 e di 5 (fizz e buzz)

---
+++ js/app.js
@@ -0,0 +1,28 @@
+// Stampare in console numeri da 1 a 100
+let n = 100
+const numeriElement = document.getElementById("numeri")
+const numeri5Element = document.getElementById("numeri")
+
+for (let i = 0; i < 100; i++){
+    let num = i + 1
+    let multiplo3 = num % 3
+    let multiplo5 = num % 5
+
+    // console.log(num)
+
+    // Per i multipli di 3 stampare "Fizz" al posto del numero
+    if (multiplo3 === 0){
+        num = "Fizz"
+        numeriElement.innerHTML += `<li> ${num} </li>`
+        console.log(num)
+      // Per i multipli di 5 stampare "Buzz" al posto del numero
+    } else if (multiplo5 === 0){
+               num = "Buzz"
+               numeri5Element.innerHTML += `<li>${num}</li>`
+               console.log(num)
+
+    }
+            
+}
+// Per i numeri che sono sia multipli di 3 che di 5 stampare "FizzBuzz" al posto del numero
+
